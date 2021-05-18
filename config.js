4b474315dfb8c95dac010004e6b2b946207417ae
Kang Xinyang <K2973749359@outlook.com>
2021-05-18 23:11:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,9 +33,9 @@ var config = {
         turn_on: "开始",
         play: "音乐",
         bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        balloons_flying: "@#@",
+        cake_fadein: "蛋糕",
+        light_candle: "蜡烛",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
